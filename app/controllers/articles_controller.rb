8a3e0ce04ce11d0031d4c1b7c6f47cea5f2ea9de
Louis Charbonnier <charbonnierlouis@gmail.com>
2023-02-21 17:01:36 +0100

    wikinimous

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :show, :destroy, :edit, :update]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params_article)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(params_article)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def params_article
+    params.require(:article).permit(:title, :content)
+  end
+end
